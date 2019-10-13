92aa9763a65c7d225f702bf86207d3acdb6bc42b
Adam Probert <adamprobert@live.co.uk>
2019-10-13 16:09:58 +0100

    Added and commented out play services for now.

--- Assets/GetHighScore.cs
+++ Assets/GetHighScore.cs
@@ -6,27 +6,27 @@ using UnityEngine.UI;
 public class GetHighScore : MonoBehaviour
 {
 
-    private GameServicesHandler gs;
+    //private GameServicesHandler gs;
     private StorageHandler sh;
     private Text text;
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<Text>();
-        gs = transform.root.GetComponent<GameServicesHandler>();
+        //gs = transform.root.GetComponent<GameServicesHandler>();
         sh = transform.root.GetComponent<StorageHandler>();
-        long highscore = gs.GetHighScore();
+        //long highscore = gs.GetHighScore();
 
-        if(highscore != -1)
-        {
-            Debug.Log("Getting high score from GameServices");
-            text.text = "Highscore\n" + highscore.ToString();
-        }
-        else
-        {
-            Debug.Log("Getting high score from storage");
-            text.text = "Highscore\n" + sh.GetHighScore().ToString();
-        }
+        //if(highscore != -1)
+        //{
+        //    Debug.Log("Getting high score from GameServices");
+        //    text.text = "Highscore\n" + highscore.ToString();
+        //}
+        //else
+        //{
+        Debug.Log("Getting high score from storage");
+        text.text = "Highscore\n" + sh.GetHighScore().ToString();
+        //}
         
     }
 }
